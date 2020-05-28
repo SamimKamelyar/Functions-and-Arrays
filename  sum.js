d05789f68575deb6029b640e794b6357f2ccc3e6
Samim Kamleyar <ahmadsamimkamelyar@gmail.com>
2020-05-28 10:56:02 -0700

    Sum array excercise

---
+++  sum.js
@@ -0,0 +1,10 @@
+function sum(arr) {
+  let total = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'number') {
+      total = total + arr[i]
+      // total += arr[i]
+    }
+  }
+  return total
+}
